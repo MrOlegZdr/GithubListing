350cde46a1bb6fa242c0948402217e2245d28afc
MrOleg <mrdrozdilla@gmail.com>
2024-08-01 20:57:50 +0200

    Initial commit

---
+++ src/main/java/com/home/project/githublisting/GithublistingApplication.java
@@ -0,0 +1,13 @@
+package com.home.project.githublisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithublistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithublistingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/project/githublisting/GithublistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.project.githublisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithublistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
